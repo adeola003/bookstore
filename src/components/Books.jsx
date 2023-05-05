4a1fa50a30ffc92c3412d6a2bbd548f9f645b321
Abdel-Khafid kolawole Fifa SALAOU <119133987+adeola003@users.noreply.github.com>
2023-05-05 21:06:24 +0100

    add components and routes

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,5 +1,17 @@
-const Books = () => (
-    <div>Books</div>
+import Form from "./Form";
+const Books = ({title, author}) => (
+   <>
+    <div>
+      <ul>
+        <li>Title : {title}</li>
+        <li>Author : {author}</li>
+      </ul>
+      <button type="submit" className="remove">remove</button>
+    </div>
+    <div>
+      <Form />
+    </div>
+    </>
   );
   
   export default Books;
