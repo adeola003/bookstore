a5ec87a5d038f82826ab62f98bb378708a707ce2
Abdel-Khafid kolawole Fifa SALAOU <119133987+adeola003@users.noreply.github.com>
2023-05-11 16:10:54 +0100

    insert the addBook function in the form using dispatch

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,15 +1,37 @@
-// import { useDispatch } from 'react-redux';
-// import { addBook } from '../redux/books/booksSlice';
-// const dispacth = useDispatch();
-const Form = () => (
-  <form action="" id="books-form">
-    <label htmlFor="Title">
-      <input type="text" id="title" placeholder="Enter the book title" />
-    </label>
-    <label htmlFor="Author">
-      <input type="text" id="auhor" placeholder="Enter the author's name" />
-    </label>
-    <button className="submit-btn" type="submit">Submit</button>
-  </form>
-);
+import { useDispatch } from 'react-redux';
+import { addBook } from '../redux/books/booksSlice';
+
+const Form = () => {
+  const dispacth = useDispatch();
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if(event.target.title.value && event.target.author.value){
+      const newBook = {
+        item_id: `item${Date.now()}`,
+        title: event.target.title.value,
+        author: event.target.author.value,
+        category: 'Uncategorized',
+      }
+      
+      dispacth(addBook(newBook))
+      event.target.reset();
+    }
+    else {
+      alert('Enter a book title and the author')
+    }
+  };
+
+  return (
+    <form action="" id="books-form" onSubmit={handleSubmit}>
+      <label htmlFor="Title">
+        <input type="text" id="title" placeholder="Enter the book title" />
+      </label>
+      <label htmlFor="Author">
+        <input type="text" id="auhor" placeholder="Enter the author's name" />
+      </label>
+      <button className="submit-btn" type="submit" onClick={()=>dispacth(addBook())}>Submit</button>
+    </form>
+  );
+};
+
 export default Form;
